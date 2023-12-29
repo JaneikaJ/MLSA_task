79fb40d179f10438ce58da5e18a58f9c7ebd2ee4
JaneikaJ <janeikaj02@gmail.com>
2023-12-29 18:26:06 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/comp";
+import image from "../images/comp.jpg";
 
 const imageAltText = " laptop";
 
@@ -34,7 +34,7 @@ const projectList = [
     title: "My Blog Website 🎉",
     description:
       "This is my Blog Website",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    url: "https://github.com/JaneikaJ",
   },
   {
     title: "Calculator Website",
